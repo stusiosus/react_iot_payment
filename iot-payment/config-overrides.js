3db2dd055224397d36cfdcdee6160d804e63a00a
studiosus <paulsimon112@gmail.com>
2024-08-08 10:59:51 +0200

    add util

--- iot-payment/config-overrides.js
+++ iot-payment/config-overrides.js
@@ -3,7 +3,7 @@ module.exports = function override(config, env) {
     // Hinzufügen der Fallback-Option für Node.js-Module
     config.resolve.fallback = {
       ...config.resolve.fallback,
-      util: require.resolve("util/"),
+      "util": require.resolve("util/") ,
     };
   
     return config;
