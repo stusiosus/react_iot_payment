6aaa8ed168fff53788c07bcac36c256e070bdc94
studiosus <paulsimon112@gmail.com>
2024-08-08 10:55:07 +0200

    Revert "delete config override"
    This reverts commit 8885584ab61829bec082c05b96c32777878c1ab7.

---
+++ iot-payment/config-overrides.js
@@ -0,0 +1,11 @@
+// config-overrides.js
+module.exports = function override(config, env) {
+    // Hinzufügen der Fallback-Option für Node.js-Module
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      util: require.resolve("util/"),
+    };
+  
+    return config;
+  };
+  
